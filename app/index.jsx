136beeb50192cb5d5ac09c43537e6f96be2d9c53
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-04-17 15:27:43 +0530

    setup initial boilerplate

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-3xl text-red-500">Home Page</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./<custom_dir>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+};
